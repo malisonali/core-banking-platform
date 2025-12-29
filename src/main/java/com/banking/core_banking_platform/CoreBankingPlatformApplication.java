07ba90ce3a13431024ade1e852eaca91c24f8900
malisonali <sonalipmali@gmail.com>
2025-12-29 20:27:04 +0530

    Added src folder and pom.xml

---
+++ src/main/java/com/banking/core_banking_platform/CoreBankingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.banking.core_banking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingPlatformApplication.class, args);
+	}
+
+}
